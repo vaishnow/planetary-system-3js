fd11a770181dd87db825687783acd29b8db0931e
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-14 16:15:59 +0530

    fixed an error in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
-  content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
