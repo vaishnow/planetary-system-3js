81f1f9a2857ec70b90b192c4edecaf3127b92967
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-18 14:55:04 +0530

    add PointLight and AmbientLight to scene

--- app/PlanetarySystem.tsx
+++ app/PlanetarySystem.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as THREE from "three";
 import { useEffect } from "react";
 import useCamera from "@/hooks/three/useCamera";
 import usePlanet from "@/hooks/three/usePlanet";
@@ -13,7 +14,9 @@ function PlanetarySystem() {
 
   // Sun
   const { sphere: sun } = useSphere({ color: "#fb2", radius: 2 });
+  sun.add(new THREE.PointLight("#fff",1000,100))
   scene.add(sun);
+  scene.add(new THREE.AmbientLight("#fff",.5));
 
   // Planets
   const [planet1, p1Start] = usePlanet({ orbitRadius: 4, planetRadius: 0.3 });
