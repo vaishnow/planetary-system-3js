c6686c806791b274ec57fd6d75b193ea8bba3c1a
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-18 12:29:50 +0530

    fixed jerky revoution speed of planets
    man i feel stupid

--- hooks/three/usePlanet.ts
+++ hooks/three/usePlanet.ts
@@ -15,16 +15,17 @@ function usePlanet({
   planet.add(orbit);
   planet.add(sphere);
 
+  // Just making random speeds for planet rotation
+  let speed = -Math.random() * 0.015;
+  if (Math.random() > 0.5) speed /= 10;
+
   const revolve = () => {
-    // Just making random speeds for planet rotation
-    let speed = -Math.random() * 0.015;
-    if (Math.random() > 0.5) speed /= 10;
     planet.rotateY(speed);
   };
 
   // Giving a slight tilt to Orbits
-  planet.rotateZ(Math.random()*.07)
-  planet.rotateX(Math.random()*.07)
+  planet.rotateZ(Math.random() * 0.07);
+  planet.rotateX(Math.random() * 0.07);
 
   return [planet, revolve];
 }
