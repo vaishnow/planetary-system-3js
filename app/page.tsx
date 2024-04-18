0c20aa223aa9e5018b71b48cd76f5ca47e8abc9f
vaishnow <142155359+vaishnow@users.noreply.github.com>
2024-04-18 09:32:26 +0530

    disabled ssr for three to work without issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
-import PlanetarySystem from "./PlanetarySystem";
+import dynamic from "next/dynamic";
+
+const PlanetarySystem = dynamic(() => import("./PlanetarySystem"), {
+  ssr: false,
+});
 
 export default function Home() {
   return <PlanetarySystem />;
